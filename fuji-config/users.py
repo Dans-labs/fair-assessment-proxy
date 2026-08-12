5196829eb958c86e9760f32a500ee56620e0d915
Reggie Cushing <reggie.cushing@dans.knaw.nl>
2026-08-12 14:33:47 +0200

    refactor(fuji-config/users.py): update user credentials management  Load environment variables for username and password to enhance security an[2D[K and flexibility.

--- fuji-config/users.py
+++ fuji-config/users.py
@@ -1,1 +1,7 @@
-fuji_users = {"fair-service": "change-this-password"}
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+fuji_users = {os.environ["FUJI_USERNAME"]: os.environ["FUJI_PASSWORD"]}
